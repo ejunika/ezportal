cbf15dd91df59c46fbd59494fa8f82c4a27809c8
akhtara <azaz.akhtar@powerschool.com>
2017-05-11 20:53:18 +0530

    Initial Commit

---
+++ com.ez.portal.osgi.core.rest/src/main/java/com/ez/portal/osgi/core/rest/service/impl/PortalServiceImpl.java
@@ -0,0 +1,22 @@
+package com.ez.portal.osgi.core.rest.service.impl;
+
+import javax.ws.rs.core.Request;
+import javax.ws.rs.core.Response;
+
+import com.ez.portal.rest.service.PortalService;
+
+public class PortalServiceImpl implements PortalService {
+
+    @Override
+    public Response get(Long id) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Response post(Request request) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
